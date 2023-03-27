199ebe88be42d2e8f1fab2cde1c2079596a311e6
Surya152002 <108388181+Surya152002@users.noreply.github.com>
2023-03-27 16:00:57 +0530

    Update process.py

--- process.py
+++ process.py
@@ -1,23 +1,39 @@
 import cv2
+import numpy as np
+from flask import Flask, render_template, Response
 
-cap = cv2.VideoCapture(0)
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+def detect_plate(frame):
+    # Perform image processing and detect the number plate here
+    # Return the processed image with the number plate highlighted
     
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    # Example code to draw a rectangle around a detected number plate
+    x, y, w, h = 100, 100, 200, 50
+    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
     
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    cv2.imshow("Frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    return frame
 
-cap.release()
-cv2.destroyAllWindows()
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            frame = detect_plate(frame)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
